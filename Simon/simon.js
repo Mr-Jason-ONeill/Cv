fff850b4d0a9e6d4c9fe11a106aae1899c19a692
Jason <jason.a.oneill@gmail.com>
2022-07-21 09:48:38 +0100

    file sort out

--- Simon/simon.js
+++
@@ -1,9 +0,0 @@
-var buttonColors = ["green", "red", "yellow", "blue"];
-var gamePattern = [];
-var randomChosenColor = buttonColors[randomNumber];
-
-function nextSequence () {
-    var randomNumber = Math.floor(Math.random() * 4);
-    gamePattern.push(randomChosenColor);
-}
-
